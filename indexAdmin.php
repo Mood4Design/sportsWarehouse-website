bcc598861d12dbfd42f62333a5efa030a7ef92f1
Allan Lee <allan.lee@studytafensw.edu.au>
2025-06-10 16:05:16 +1000

    indexAdmin update

--- indexAdmin.php
+++ indexAdmin.php
@@ -1,16 +1,66 @@
 <?php
 
+  // Common includes for main PHP pages (controllers)
+  require_once "includes/common.php";
+
   // Config
   $title = "Home";
 
-  // Start output buffering (trap output, don't display it yet)
+  // Start output buffering (trap output - don't display it yet)
   ob_start();
 
   // Include the page-specific template
   include_once "./templates/_homePage.html.php";
 
-  // Stop output buffering - store output into our $output variable
-  $output = ob_get_clean();
 
-  // Include layout template
-  include_once "./templates/_layoutAdmin.html.php";
+  /*
+   * Search for products < $20
+   */
+
+  // Define SQL query
+  // LIMIT 0, 6 means: skip 0 rows (0 offset), only return 6 rows (count)
+  $sql = <<<SQL
+    SELECT	itemId, itemName, categoryId, price, salePrice, description, photo
+    FROM	  item
+    WHERE   price < 20
+    LIMIT   0, 6
+  SQL;
+
+  // Prepare the statement
+  $stmt = $db->prepareStatement($sql);
+
+  // Bind values (if needed)
+  // $stmt->bindValue(":categoryId", $categoryId, PDO::PARAM_INT);
+
+  // Execute query (get the list of products under $20)
+  $productsUnder20 = $db->executeSQL($stmt);
+
+  /*
+   * Search for products > $50
+   */
+
+  // Define SQL query
+  $sql = <<<SQL
+    SELECT	itemId, itemName, categoryId, price, salePrice, description, photo
+    FROM	  item
+    WHERE   price >= 0
+    LIMIT   0, 6
+  SQL;
+
+  // Prepare the statement
+  $stmt = $db->prepareStatement($sql);
+
+  // Execute query (get the list of products over $50)
+  $productsOver50 = $db->executeSQL($stmt);
+
+  // Include the page-specific template
+  include_once "templates/_indexPage.html.php";
+
+  // Stop output buffering - store output into the $content variable
+  $content = ob_get_clean();
+
+  // Include the main layout template
+  include_once "templates/_layout.html.php";
+
+
+ 
--- templates/_homePage.html.php
+++ templates/_homePage.html.php
@@ -1,8 +1,10 @@
 <h2>Home - testing the Auth class</h2>
-
+<div class="container">
+            <h3>Admin</h3>
 <p>This website tests the Auth class that handles authentication (login/logout) and authorisation (checking if a user is logged in). This page is not protected so that anyone can access it, but you will need to be logged in to access the <code>protected.php</code> page.</p>
 
 <p>Once logged in, the user's details are stored in a PHP session, which is checked when they are trying to access a page protected with <code>Auth::protect()</code>.</p>
+
 
 <ul>
   <li><a href="createUser.php">Create user</a> (do NOT have this accessible on a production website!)</li>
@@ -10,3 +12,4 @@
   <li><a href="logout.php">Logout</a></li>
   <li><a href="protected.php">Protected page</a> (does NOT allow access to unauthorised users)</li>
 </ul>
+</div>
