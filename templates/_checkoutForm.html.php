065e68395d68902dcda920b60fd52b0ce69893f1
Allan Lee <leallan69@gmail.com>
2025-06-18 19:46:26 +1000

    Update

--- templates/_checkoutForm.html.php
+++ templates/_checkoutForm.html.php
@@ -1,48 +1,50 @@
+
 <div class="container">
-  <h3>Checkout</h3>
-  <form method="post" action="thanks.php" id="checkout">
-<fieldset>
-<legend>Delivery Details</legend>
-<p>
-<label for="firstName">First Name:</label>
-<input type="text" id="firstName" name="firstName">
-</p>
-<p>
-<label for="lastName">Last Name:</label>
-<input type="text" id="lastName" name="lastName">
-</p>
-<p>
-<label for="address">Address:</label>
-<input type="text" id="address" name="address">
-</p>
-<p>
-<label for="phone">Phone:</label>
-<input type="text" id="phone" name="phone">
-</p>
-<p>
-<label for="email">Email:</label>
-<input type="text" id="email" name="email">
-</p>
-</fieldset>
-<fieldset>
-<legend>Payment</legend>
-<p>
-<label for="creditcard">Credit card number:</label>
-<input type="text" id="creditcard" name="creditcard">
-</p>
-<p>
-<label for="nameOnCard">Name on card:</label>
-<input type="text" id="nameOnCard" name="nameOnCard">
-</p>
-<p>
-<label for="expiry">Expiry date:</label>
-<input type="text" id="expiry" name="expiry">
-</p>
-<p>
-<label for="csv">CSV:</label>
-<input type="text" id="csv" name="csv">
-</p>
-<p><input type="submit" name="pay" value="Pay"></p>
-</fieldset>
-</form>
+    <h3>Checkout</h3>
+        <form method="post" action="thanks.php" id="checkout">
+                <fieldset>
+                    <legend>Delivery Details</legend>
+                        <p>
+                        <label for="firstName">First Name:</label>
+                        <input type="text" id="firstName" name="firstName">
+                        </p>
+                        <p>
+                        <label for="lastName">Last Name:</label>
+                        <input type="text" id="lastName" name="lastName">
+                        </p>
+                        <p>
+                        <label for="address">Address:</label>
+                        <input type="text" id="address" name="address">
+                        </p>
+                        <p>
+                        <label for="phone">Phone:</label>
+                        <input type="text" id="phone" name="phone">
+                        </p>
+                        <p>
+                        <label for="email">Email:</label>
+                        <input type="text" id="email" name="email">
+                       </p>
+                </fieldset>
+
+                <fieldset>
+                      <legend>Payment</legend>
+                        <p>
+                        <label for="creditcard">Credit card number:</label>
+                        <input type="text" id="creditcard" name="creditcard">
+                        </p>
+                        <p>
+                        <label for="nameOnCard">Name on card:</label>
+                        <input type="text" id="nameOnCard" name="nameOnCard">
+                        </p>
+                        <p>
+                        <label for="expiry">Expiry date:</label>
+                        <input type="text" id="expiry" name="expiry">
+                        </p>
+                        <p>
+                        <label for="csv">CSV:</label>
+                        <input type="text" id="csv" name="csv">
+                        </p>
+                        <p><input type="submit" name="pay" value="Pay"></p>
+                  </fieldset>
+          </form>
 </div>
