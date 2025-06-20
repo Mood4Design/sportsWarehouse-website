6dffdbde6f2cc702abaf94f26a3d2abb018fa38b
Allan Lee <leallan69@gmail.com>
2025-06-20 13:34:07 +1000

    Update _displayItems.html.php

--- templates/_displayItems.html.php
+++ templates/_displayItems.html.php
@@ -7,26 +7,29 @@
                     $itemId = $row["itemId"];
                     $photoPath = isset($row["photo"]) ? "image/" . $row["photo"] : "";
                     $price = isset($row["salePrice"]) && $row["salePrice"] > 0 ? $row["salePrice"] : $row["price"];?>
-                
-                            <article class="product">
-                                <form class="product__link" action="product.php" method="post">
-                                        <img src="<?= $photoPath ?>" alt="">
-                                        <p class="info"><?= $productName ?></p>
-                                        <p class="current-price">
-                                            <?php if (isset($row['salePrice']) && $row['salePrice'] > 0): ?>
-                                                <?= sprintf('$%1.2f', $row['salePrice']) ?>
-                                                <span class="was-text">WAS</span>
-                                                <span class="old-price"><del><?= sprintf('$%1.2f', esc($row['price'])) ?></del></span>
-                                            <?php else: ?>
-                                                <span class="old-price"><?= sprintf('$%1.2f', esc($row['price'])) ?></span>
-                                            <?php endif; ?>
-                                        </p>
-                                        <p><label for="qty<?=$itemId?>">quantity:</label>
-                                        <input class="qty" style="width: 80px; text-align: center;" type="number" id="qty<?=$itemId?>" name="qty" value="1">
-                                        </p>
-                                        <p><input class="buy" type="submit" name="buy" value="Buy"></p>
-                                        <input type="hidden" value="<?=$itemId?>" name="itemId">
-                                </form>
+                <article class="product">
+                                                 <a class="product__link" href="product.php?id=<?=$itemId?>">
+                                                         <img src="<?= $photoPath ?>" alt="">
+                                                         <p class="info"><?= $productName ?></p>
+                                                         <p class="current-price">
+                                                             <?php if (isset($row['salePrice']) && $row['salePrice'] > 0): ?>
+                                                                 <?= sprintf('$%1.2f', $row['salePrice']) ?>
+                                                                 <span class="was-text">WAS</span>
+                                                                 <span class="old-price"><del><?= sprintf('$%1.2f', esc($row['price'])) ?></del></span>
+                                                             <?php else: ?>
+                                                                 <span class="old-price"><?= sprintf('$%1.2f', esc($row['price'])) ?></span>
+                                                             <?php endif; ?>
+                                                         </p>
+                                                         
+                                                 </a>
+                                                 <form action="cart.php" method="post">
+                                                  <p><label for="qty<?=$itemId?>">quantity:</label>
+                                                         <input class="qty" style="width: 80px; text-align: center;" type="number" id="qty<?=$itemId?>" name="qty" value="1">
+                                                         </p>
+                                                         <p><input class="buy" type="submit" name="buy" value="Buy"></p>
+                                                         <input type="hidden" value="<?=$itemId?>" name="itemId">
+                                                 </form>
+                                                
                             </article>
                 <?php endforeach; ?>
     
